d00b28411b97e8d30d5b6a6e4388791aee39ac93
Anatolii Ivanov <gotnickname@gmail.com>
2023-06-01 21:21:11 +0530

    Add project files.

---
+++ SkillFactory.Module14Linq1/BookUI.cs
@@ -0,0 +1,18 @@
+﻿namespace SkillFactory.Module14Linq1
+{
+    public static class BookUI
+    {
+        public static string Intstructions = "Press 1, 2, 3 for viewing phonebook pages or Q to exit the app.\n";
+        private static int _contactPerPage = 2;
+
+        public static void ShowPage(int page, List<Contact> contacts)
+        {
+            Console.Clear();
+            Console.WriteLine(Intstructions);
+            var contactpage = contacts.Skip((page - 1) * _contactPerPage)
+                                      .Take(_contactPerPage)
+                                      .Select(x => $"{x.Name} {x.LastName}: {x.PhoneNumber}");
+            Console.WriteLine(String.Join('\n', contactpage));
+        }
+    }
+}
---
+++ SkillFactory.Module14Linq1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace SkillFactory.Module14Linq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dummy data. 
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            // Ordering list in alphabetical order.
+            phoneBook = phoneBook.OrderBy(x => x.Name)
+                                 .ThenBy(x => x.LastName)
+                                 .ToList();
+            Console.WriteLine(BookUI.Intstructions);
+            int pageToShow = 1;
+            while (true)
+            {
+                pageToShow = Console.ReadKey(true).Key switch
+                {
+                    ConsoleKey.D1 => 1,
+                    ConsoleKey.D2 => 2,
+                    ConsoleKey.D3 => 3,
+                    // Adding option to break on.
+                    ConsoleKey.Q  => 0,
+                    _             => pageToShow,
+                };
+                if (pageToShow == 0) break;
+
+                BookUI.ShowPage(pageToShow, phoneBook);
+            }
+        }
+    }
+}
